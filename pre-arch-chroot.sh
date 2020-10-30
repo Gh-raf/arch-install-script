fe5c6fec3fa6adfa6bbe8955b842b60726ad15ee
Gh-raf <71462480+Gh-raf@users.noreply.github.com>
2020-10-30 10:32:16 -0400

    Update pre-arch-chroot.sh

--- pre-arch-chroot.sh
+++ pre-arch-chroot.sh
@@ -16,7 +16,7 @@ root='30720'
 
 cal() { awk "BEGIN{print $*}"; }
 dec () { cal $* - $sctr_l; }
-MiB_B () { cal $1 \* 1048576; }
+mib_b () { cal $1 \* 1048576; }
 x () { echo /dev/$blk_dev$1; }
 
 ##################################
@@ -27,9 +27,9 @@ x () { echo /dev/$blk_dev$1; }
 [ -d /sys/firmware/efi ] || (echo 'Non-UEFI boot modes aren'\''t supported! Aborting...' && sleep 5 && exit)
 
 # Calculate disk space for each partition
-boot_end=$(cal 2048 \* sctr_l + $(MiB_B $boot))B
-swap_end=$(cal $boot_end + $(MiB_B $swap))B
-root_end=$(cal $swap_end + $(MiB_B $root))B
+boot_end=$(cal 2048 \* sctr_l + $(mib_b $boot))
+swap_end=$(cal $boot_end + $(mib_b $swap))
+root_end=$(cal $swap_end + $(mib_b $root))
 
 # Sync sys clock
 timedatectl set-ntp true
@@ -37,11 +37,11 @@ timedatectl set-ntp true
 # Create and Format the partitions
 parted $(x) -- mklabel gpt
 parted -s $(x) -- \
-	 mkpart boot fat32 		2048s $(dec $boot_end) \
+	 mkpart boot fat32 		2048s $(dec $boot_end)B \
 	 set 1 boot on \
-	 mkpart swap linux-swap $boot_end $(dec $swap_end)  \
-	 mkpart root ext4 		$swap_end $(dec $root_end) \
-	 mkpart home ext4 		$root_end -34s
+	 mkpart swap linux-swap 	"$boot_end"B $(dec $swap_end)B  \
+	 mkpart root ext4 		"$swap_end"B $(dec $root_end)B \
+	 mkpart home ext4 		"$root_end"B -34s
 mkfs.fat -F32 $(x 1)
 mkswap $(x 2)
 swapon $(x 2)
