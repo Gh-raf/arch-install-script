32dce179531fa22784b8e33a261f328fa60187c2
Gh-raf <71462480+Gh-raf@users.noreply.github.com>
2020-12-05 16:38:24 +0000

    Update pre-arch-chroot.sh

--- pre-arch-chroot.sh
+++ pre-arch-chroot.sh
@@ -1,15 +1,16 @@
 set -e
 
-blk_dev+='sda'
-sctr_l+=$(cat /sys/block/$blk_dev/queue/hw_sector_size)
-boot+='512'
-swap+='8192'
-root+='30720'
+sdx+=sda
+sctr_l+=$(cat /sys/block/$sdx/queue/hw_sector_size)
+boot+=512
+swap+=8192
+root+=30720
+
+blk_dev=/dev/"$sdx"
 
 cal() { awk "BEGIN{print $*}"; }
 dec () { cal $* - $sctr_l; }
 MiB_B () { cal $1 \* 1048576; }
-x () { echo /dev/$blk_dev$1; }
 
 ##################################
 ####     Pre-installation     ####
@@ -27,24 +28,24 @@ root_end=$(cal $swap_end + $(MiB_B $root))
 timedatectl set-ntp true
 
 # Create and Format the partitions
-parted $(x) mklabel gpt
-parted -a optimal -s $(x) -- \
+parted $blk_dev mklabel gpt
+parted -a optimal -s $blk_dev -- \
 	 mkpart boot fat32 		2048s $(dec $boot_end)B \
 	 set 1 boot on \
 	 mkpart swap linux-swap "$boot_end"B $(dec $swap_end)B  \
 	 mkpart root ext4 		"$swap_end"B $(dec $root_end)B \
 	 mkpart home ext4 		"$root_end"B -34s
 
-mkfs.fat -F32 $(x 1)
-mkswap $(x 2)
-swapon $(x 2)
-mkfs.ext4 $(x 3)
-mkfs.ext4 $(x 4)
+mkfs.fat -F32 "$blk_dev"1
+mkswap "$blk_dev"2
+swapon "$blk_dev"2
+mkfs.ext4 "$blk_dev"3
+mkfs.ext4 "$blk_dev"4
 
 # Mount and Setup Dirs
-mount $(x 3) /mnt
-mkdir -p /mnt/boot && mount $(x 1) /mnt/boot
-mkdir /mnt/home && mount $(x 4) /mnt/home
+mount "$blk_dev"3 /mnt
+mkdir -p /mnt/boot && mount "$blk_dev"1 /mnt/boot
+mkdir /mnt/home && mount "$blk_dev"4 /mnt/home
 
 # Install Base System (tty) + man pages + text editor
 pacstrap /mnt base base-devel linux linux-firmware linux-zen man-db man-pages git
