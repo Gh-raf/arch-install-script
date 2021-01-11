609c6dab941b793c16e4fd5ff22297186aed839d
Gh-raf <71462480+Gh-raf@users.noreply.github.com>
2021-01-11 19:28:50 +0000

    Update pre-chroot.sh

--- pre-chroot.sh
+++ pre-chroot.sh
@@ -8,9 +8,10 @@ root+=30720
 
 blk_dev=/dev/"$sdx"
 
-cal() { awk "BEGIN{print $*}"; }
+cal () { awk "BEGIN{print $*}"; }
 dec () { cal $* - $sctr_l; }
 MiB_B () { cal $1 \* 1048576; }
+mnt () { mkdir -p $2 && mount $1 $2; }
 
 ##################################
 ####     Pre-installation     ####
@@ -27,11 +28,11 @@ root_end=$(cal $swap_end + $(MiB_B $root))
 # Create and Format the partitions
 parted $blk_dev mklabel gpt
 parted -a optimal -s $blk_dev -- \
-	 mkpart boot fat32 		2048s $(dec $boot_end)B \
+	 mkpart boot	fat32		2048s $(dec $boot_end)B \
 	 set 1 boot on \
-	 mkpart swap linux-swap "$boot_end"B $(dec $swap_end)B  \
-	 mkpart rootfs ext4 		"$swap_end"B $(dec $root_end)B \
-	 mkpart home ext4 		"$root_end"B -34s
+	 mkpart swap	linux-swap	"$boot_end"B $(dec $swap_end)B  \
+	 mkpart rootfs	ext4 		"$swap_end"B $(dec $root_end)B \
+	 mkpart home	ext4 		"$root_end"B -34s
 
 mkfs.fat -F32 "$blk_dev"1
 mkswap "$blk_dev"2
@@ -40,11 +41,11 @@ mkfs.ext4 "$blk_dev"3
 mkfs.ext4 "$blk_dev"4
 
 # Mount and Setup Dirs
-mount "$blk_dev"3 /mnt
-mkdir -p /mnt/boot && mount "$blk_dev"1 /mnt/boot
-mkdir /mnt/home && mount "$blk_dev"4 /mnt/home
+mnt "$blk_dev"3 /mnt
+mnt "$blk_dev"1 /mnt/boot
+mnt "$blk_dev"4 /mnt/home
 
 # Install Base System (tty) + man pages + text editor
 basestrap /mnt base base-devel runit elogind-runit linux linux-zen linux-firmware man-db man-pages
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab || echo No \`genfstab\`, pleaze fill manually your \`/etc/fstab\` and \`artix-chroot /mnt\`
 artix-chroot /mnt
